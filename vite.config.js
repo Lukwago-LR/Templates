db96ca114b5181376363d97c9909ce2fb4f1c195
Lukwago-LR <lukwagorsaved@gmail.com>
2023-05-27 22:10:24 +0200

    add

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import data from "./data.json" assert {type: 'json'};
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        sub1: 'index_Bootstrap.html',
+        sub2: 'index_Materialize.html'
+      }
+    }
+  },
+  base : '/responsive_webpages/',
+  plugins: [handlebars({
+    context: {
+      data
+    }
+  })],
+})
